0e28970d621923a431ead4ed723273384cdfd9a2
kth <pni2396@gmail.com>
2026-07-03 16:31:54 +0900

    fix: Review 엔티티 테이블 명 수정 및 필드레벨 Builder 적용

--- src/main/java/com/sparta/ordering/review/entity/Review.java
+++ src/main/java/com/sparta/ordering/review/entity/Review.java
@@ -2,21 +2,28 @@ package com.sparta.ordering.review.entity;
 
 import com.sparta.ordering.global.entity.BaseUpdatableEntity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@Table(name = "t_reviews")
+@Table(name = "p_reviews")
 @Entity
 public class Review extends BaseUpdatableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column
     private UUID id;
+
+    @Builder
+    public Review(int rating, String comment) {
+        this.rating = rating;
+        this.comment = comment;
+    }
 
     //@Column
     //private Order orderId;
