92d92a8a4fc2645d2fd59d0e7c4b4ea83ab3d3ce
soyul <soyul9280@gmail.com>
2026-07-03 15:39:49 +0900

    feat: User entity & repository생성

---
+++ src/main/java/com/sparta/ordering/user/entity/User.java
@@ -0,0 +1,45 @@
+package com.sparta.ordering.user.entity;
+
+import com.sparta.ordering.global.entity.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name = "p_users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseUpdatableEntity {
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Role role;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String userName, String nickName, String phoneNumber, Role role, String password) {
+        this.userName = userName;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/com/sparta/ordering/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.ordering.user.repository;
+
+import com.sparta.ordering.user.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
