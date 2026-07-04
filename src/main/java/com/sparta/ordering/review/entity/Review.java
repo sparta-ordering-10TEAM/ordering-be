d0050fe78569371c73b1c65a5e6e78cf311a6cde
kth <pni2396@gmail.com>
2026-07-04 22:05:02 +0900

    refactor: Review 엔티티 수정
    - User JPA ManyToOne 맵핑 및 on delete cascade 제약조건 부여

--- src/main/java/com/sparta/ordering/review/entity/Review.java
+++ src/main/java/com/sparta/ordering/review/entity/Review.java
@@ -1,24 +1,28 @@
 package com.sparta.ordering.review.entity;
 
 import com.sparta.ordering.global.entity.BaseUpdatableEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.sparta.ordering.user.entity.User;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "p_reviews")
 @Entity
 public class Review extends BaseUpdatableEntity {
-    //@Column
-    //private Order orderId;
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "order_id", nullable = false)
+    //private Order order;
 
-    //@Column
-    //private User customerId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", columnDefinition = "uuid", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
 
     @Column
     private int rating;
@@ -27,7 +31,8 @@ public class Review extends BaseUpdatableEntity {
     private String comment;
 
     @Builder
-    public Review(int rating, String comment) {
+    public Review(User user, int rating, String comment) {
+        this.user = user;
         this.rating = rating;
         this.comment = comment;
     }
