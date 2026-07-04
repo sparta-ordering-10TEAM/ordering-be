25962b54cd1b2d7212dfc916d8f5b63fd7bad2b2
wontae <dnjsxo0616@gmail.com>
2026-07-04 20:22:04 +0900

    feat: 주문 엔티티 생성

---
+++ src/main/java/com/sparta/ordering/order/entity/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.sparta.ordering.order.entity;
+
+public enum OrderStatus {
+    REQUESTED,
+    APPROVED,
+    COOKING_COMPLETED,
+    DELIVERING,
+    COMPLETED,
+    CANCELLED,
+    REJECTED
+}
