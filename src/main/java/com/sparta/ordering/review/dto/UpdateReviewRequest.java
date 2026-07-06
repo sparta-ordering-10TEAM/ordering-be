e5edd784e12b11373f39e31ec242d89e42718021
kth <pni2396@gmail.com>
2026-07-06 17:51:36 +0900

    fix: 리뷰 수정 API RequestBody 누락 수정

---
+++ src/main/java/com/sparta/ordering/review/dto/UpdateReviewRequest.java
@@ -0,0 +1,13 @@
+package com.sparta.ordering.review.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+public record UpdateReviewRequest(
+        @Min(value = 1, message = "평점은 최소 1점 이상만 가능합니다.")
+        @Max(value = 5, message = "평점은 최대 1점 이하만 가능합니다.")
+        Integer rating,
+
+        String comment
+) {
+}
