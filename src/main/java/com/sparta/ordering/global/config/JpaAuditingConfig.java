44bb495a85cf2e7f318d521cdd6bd896d585a650
soyul <soyul9280@gmail.com>
2026-07-06 13:53:19 +0900

    refactor: Jpa Auditing AuditorAware 구현

--- src/main/java/com/sparta/ordering/global/config/JpaAuditingConfig.java
+++ src/main/java/com/sparta/ordering/global/config/JpaAuditingConfig.java
@@ -6,15 +6,13 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Optional;
 import java.util.UUID;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
-/*
+
     @Bean
     public AuditorAware<UUID> auditorProvider() {
         return () -> {
@@ -26,9 +24,8 @@ public class JpaAuditingConfig {
                 return Optional.empty();
             }
 
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            return Optional.of(userDetails.getUserId());
+            return Optional.of((UUID) authentication.getPrincipal());
         };
     }
- */
+
 }
