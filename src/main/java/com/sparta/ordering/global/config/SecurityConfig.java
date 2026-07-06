6146db31cb4c1d378698e3eee8b2e9b5fb5db83f
kth <pni2396@gmail.com>
2026-07-06 17:50:41 +0900

    fix: JwtTokenProvider 환경변수 사용 방식 변경
    - 코드 레벨에서 yml 프로필이 아닌 .env 파일을 바로 참조하는 문제 수정
    - SecurityConfig import 누락 수정

--- src/main/java/com/sparta/ordering/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/ordering/global/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.sparta.ordering.global.config;
 import com.sparta.ordering.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
