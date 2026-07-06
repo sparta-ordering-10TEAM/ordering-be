abcc54f4f39f753382e170eda36a5508f451a328
wootae <comijoa2@naver.com>
2026-07-06 15:23:09 +0900

    chore: Product 엔티티 import 와일드카드 제거

--- src/main/java/com/sparta/ordering/product/entity/Product.java
+++ src/main/java/com/sparta/ordering/product/entity/Product.java
@@ -2,7 +2,12 @@ package com.sparta.ordering.product.entity;
 
 import com.sparta.ordering.global.entity.BaseUpdatableEntity;
 import com.sparta.ordering.restaurant.entity.Restaurant;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
