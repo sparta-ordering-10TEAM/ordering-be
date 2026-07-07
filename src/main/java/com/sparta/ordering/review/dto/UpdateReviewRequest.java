47bd6b5cc7b984142d3ee04e616d1dcd03d980e4
kth <pni2396@gmail.com>
2026-07-07 10:06:10 +0900

    fix: validation 메세지 오타 수정

--- src/main/java/com/sparta/ordering/review/dto/UpdateReviewRequest.java
+++ src/main/java/com/sparta/ordering/review/dto/UpdateReviewRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Min;
 
 public record UpdateReviewRequest(
         @Min(value = 1, message = "평점은 최소 1점 이상만 가능합니다.")
-        @Max(value = 5, message = "평점은 최대 1점 이하만 가능합니다.")
+        @Max(value = 5, message = "평점은 최대 5점 이하만 가능합니다.")
         Integer rating,
 
         String comment
