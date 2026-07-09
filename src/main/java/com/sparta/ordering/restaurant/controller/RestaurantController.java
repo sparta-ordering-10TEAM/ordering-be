ea9da3098c0c4dbd76044334e0abbde170a4ff16
ByeongGyu An <devafox62@gmail.com>
2026-07-09 23:11:49 +0900

    feat: 레스토랑 삭제 기능 컨트롤러 구현

--- src/main/java/com/sparta/ordering/restaurant/controller/RestaurantController.java
+++ src/main/java/com/sparta/ordering/restaurant/controller/RestaurantController.java
@@ -14,6 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -86,5 +87,16 @@ public class RestaurantController {
                 GeneralResponseCode.OK,
                 restaurantService.updateRestaurant(restaurantId, request, userId)
         );
+    }
+
+    @PreAuthorize("hasAnyRole('MANAGER', 'MASTER', 'OWNER')")
+    @DeleteMapping("/restaurants/{restaurantId}")
+    public ResponseEntity<GeneralResponse<Void>> deleteRestaurant(
+            @PathVariable UUID restaurantId,
+            @AuthenticationPrincipal UUID userId
+    ) {
+        restaurantService.deleteRestaurant(restaurantId, userId);
+
+        return GeneralResponse.toResponseEntity(GeneralResponseCode.OK, null);
     }
 }
