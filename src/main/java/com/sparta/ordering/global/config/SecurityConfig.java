b9f0d5e7879dd854ea8c42a8ca04121e591a7a46
soyul <soyul9280@gmail.com>
2026-07-10 20:15:30 +0900

    feat: 로그인 기능 빈 등록

--- src/main/java/com/sparta/ordering/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/ordering/global/config/SecurityConfig.java
@@ -1,10 +1,21 @@
 package com.sparta.ordering.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.ordering.auth.security.customauthentication.CustomAuthenticationFailureHandler;
+import com.sparta.ordering.auth.security.customauthentication.CustomAuthenticationFilter;
+import com.sparta.ordering.auth.security.customauthentication.CustomAuthenticationSuccessHandler;
+import com.sparta.ordering.auth.security.customauthentication.CustomUserDetailService;
+import com.sparta.ordering.auth.security.customauthentication.CustomUserDetails;
+import com.sparta.ordering.auth.security.session.JwtSessionService;
 import com.sparta.ordering.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +25,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -21,10 +34,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   CustomAuthenticationFilter customAuthenticationFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
@@ -36,13 +49,57 @@ public class SecurityConfig {
                         // TODO: 일단은 모든 요청 허용
                         .anyRequest().permitAll()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+                                                       CustomUserDetailService customUserDetailService) throws Exception{
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        //  setUserDetailsService 대신 빌더의 userDetailsService()를 사용합니다.
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailService)
+                .passwordEncoder(passwordEncoder());
+
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter(
+            ObjectMapper objectMapper,
+            AuthenticationSuccessHandler authenticationSuccessHandler,
+            AuthenticationFailureHandler authenticationFailureHandler,
+            AuthenticationManager authenticationManager) {
+
+        CustomAuthenticationFilter filter = new CustomAuthenticationFilter(objectMapper);
+        // handler 설정
+        filter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        filter.setAuthenticationFailureHandler(authenticationFailureHandler);
+        // /api/auth/sign-in 경로에 적용
+        filter.setFilterProcessesUrl("/api/auth/sign-in");
+        // authenticationManager 지정
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler(
+            ObjectMapper objectMapper, JwtSessionService jwtSessionService) {
+        return new CustomAuthenticationSuccessHandler(objectMapper, jwtSessionService);
+    }
+
+    @Bean
+    public AuthenticationFailureHandler customAuthenticationFailureHandler(ObjectMapper objectMapper) {
+        return new CustomAuthenticationFailureHandler(objectMapper);
     }
 
 }
