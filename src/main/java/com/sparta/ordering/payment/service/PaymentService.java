19f5ea2a27ed5f4c4c76fdc1b853e0d59c81edcb
wootae <comijoa2@naver.com>
2026-07-14 00:19:58 +0900

    refactor: PaymentService 역할 분기를 swtich 문으로 변경

--- src/main/java/com/sparta/ordering/payment/service/PaymentService.java
+++ src/main/java/com/sparta/ordering/payment/service/PaymentService.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -26,8 +25,6 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
-
-    private static final Set<Role> PRIVILEGED_ROLES = Set.of(Role.MANAGER, Role.MASTER);
 
     @Transactional
     public Payment preparePayment(UUID userId, PaymentRequest request) {
@@ -77,37 +74,25 @@ public class PaymentService {
 
     @Transactional(readOnly = true)
     public PaymentResponse getPayment(UUID paymentId, UUID userId, Role role) {
-
-
-        Payment payment;
-        if (PRIVILEGED_ROLES.contains(role)) {
-            payment = paymentRepository.findByIdAndDeletedAtIsNull(paymentId) // master, manager 조회 가능
+        Payment payment = switch (role) {
+            case MANAGER, MASTER -> paymentRepository.findByIdAndDeletedAtIsNull(paymentId) // master, manager 조회 가능
                     .orElseThrow(() -> new ApiException(GeneralResponseCode.PAYMENT_NOT_FOUND));
-        } else if (role.equals(Role.OWNER)) { // owner는 자기 가게 주문인지 확인
-            payment = paymentRepository.findByIdAndOrder_Restaurant_User_IdAndDeletedAtIsNull(paymentId, userId)
+            case OWNER -> paymentRepository.findByIdAndOrder_Restaurant_User_IdAndDeletedAtIsNull(paymentId, userId) // owner는 자기 가게 주문인지 확인
                     .orElseThrow(() -> new ApiException(GeneralResponseCode.PAYMENT_NOT_FOUND));
-        } else if (role.equals(Role.CUSTOMER)) { // customer는 자기 주문인지 확인
-            payment = paymentRepository.findByIdAndOrder_User_IdAndDeletedAtIsNull(paymentId, userId)
+            case CUSTOMER -> paymentRepository.findByIdAndOrder_User_IdAndDeletedAtIsNull(paymentId, userId) // customer는 자기 주문인지 확인
                     .orElseThrow(() -> new ApiException(GeneralResponseCode.PAYMENT_NOT_FOUND));
-        } else {
-            throw new ApiException(GeneralResponseCode.PAYMENT_NOT_FOUND);
-        }
+        };
 
         return PaymentResponse.from(payment);
     }
 
     @Transactional(readOnly = true)
     public Page<PaymentResponse> getPayments(UUID userId, Role role, Pageable pageable) {
-        Page<Payment> payments;
-        if (PRIVILEGED_ROLES.contains(role)) { // master, manager 조회
-            payments = paymentRepository.findAllByDeletedAtIsNull(pageable);
-        } else if (role.equals(Role.OWNER)) { // owner는 자기 가게 주문인지 확인
-            payments = paymentRepository.findAllByOrder_Restaurant_User_IdAndDeletedAtIsNull(userId, pageable);
-        } else if (role.equals(Role.CUSTOMER)) { // customer는 자기 주문인지 확인
-            payments = paymentRepository.findAllByOrder_User_IdAndDeletedAtIsNull(userId, pageable);
-        } else {
-            throw new ApiException(GeneralResponseCode.PAYMENT_NOT_FOUND);
-        }
+        Page<Payment> payments = switch (role) {
+            case MANAGER, MASTER -> paymentRepository.findAllByDeletedAtIsNull(pageable); // master, manager 조회
+            case OWNER -> paymentRepository.findAllByOrder_Restaurant_User_IdAndDeletedAtIsNull(userId, pageable); // owner는 자기 가게 주문인지 확인
+            case CUSTOMER -> paymentRepository.findAllByOrder_User_IdAndDeletedAtIsNull(userId, pageable); // customer는 자기 주문인지 확인
+        };
         return payments.map(PaymentResponse::from);
     }
 }
