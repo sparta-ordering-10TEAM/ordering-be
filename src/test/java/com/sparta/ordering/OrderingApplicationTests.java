becc3b0b66d1984fb6feeb1a03c8fa9cbc489337
soyul <soyul9280@gmail.com>
2026-07-13 19:02:10 +0900

    fix: SpringBootTest 컨테이너 적용

--- src/test/java/com/sparta/ordering/OrderingApplicationTests.java
+++ src/test/java/com/sparta/ordering/OrderingApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sparta.ordering;
 
+import com.sparta.ordering.global.config.ContainerInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test-security")
+@ContextConfiguration(initializers = ContainerInitializer.class)
 class OrderingApplicationTests {
 
     @Test
