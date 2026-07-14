8af2096b66464c54cf2622a517116f24740d3f3b
soyul <soyul9280@gmail.com>
2026-07-14 14:13:19 +0900

    refactor: 비밀번호 변경 시 인증 무효화

--- src/main/java/com/sparta/ordering/auth/service/AuthService.java
+++ src/main/java/com/sparta/ordering/auth/service/AuthService.java
@@ -8,8 +8,6 @@ import com.sparta.ordering.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
