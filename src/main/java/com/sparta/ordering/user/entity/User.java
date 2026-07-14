8158175af31acd9b37ce8c86b7a1234a78f074b9
soyul <soyul9280@gmail.com>
2026-07-14 12:04:08 +0900

    fix: User Role기본값 적용

--- src/main/java/com/sparta/ordering/user/entity/User.java
+++ src/main/java/com/sparta/ordering/user/entity/User.java
@@ -34,7 +34,7 @@ public class User extends BaseUpdatableEntity {
     @Column(nullable = false)
     private String phoneNumber;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "role DEFAULT 'CUSTOMER'")
     @Enumerated(EnumType.STRING)
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private Role role;
