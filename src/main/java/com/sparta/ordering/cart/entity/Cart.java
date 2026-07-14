51b795b9ed0e7e10a6c56787445d77fd38f3236b
wootae <comijoa2@naver.com>
2026-07-14 22:58:18 +0900

    refactor: 카트 엔티티 unique_version 컬럼 추가
    - unique_version 추가로 카트에 담긴 상품 없으면 카트 삭제
    - 빈 카트 상태에서 서로 다른 식당의 상품 추가 동시 요청 문제 해결

--- src/main/java/com/sparta/ordering/cart/entity/Cart.java
+++ src/main/java/com/sparta/ordering/cart/entity/Cart.java
@@ -3,6 +3,7 @@ package com.sparta.ordering.cart.entity;
 import com.sparta.ordering.global.entity.BaseUpdatableEntity;
 import com.sparta.ordering.restaurant.entity.Restaurant;
 import com.sparta.ordering.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
@@ -15,13 +16,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(
         name = "p_carts",
         uniqueConstraints = {
                 @UniqueConstraint(
                         name = "uk_cart_user",
-                        columnNames = {"user_id"}
+                        columnNames = {"user_id", "unique_version"}
                 )
         }
 )
@@ -29,21 +32,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart extends BaseUpdatableEntity {
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
+    @Column(name = "unique_version", nullable = false, columnDefinition = "uuid")
+    private UUID uniqueVersion;
+
     @Builder
     public Cart(User user, Restaurant restaurant) {
         this.user = user;
         this.restaurant = restaurant;
+        this.uniqueVersion = UUID.fromString("00000000-0000-0000-0000-000000000000");
     }
 
-    public void changeRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
+    @Override
+    public void softDelete(UUID deletedBy) {
+        super.softDelete(deletedBy);
+        this.uniqueVersion = this.getId();
     }
 }
